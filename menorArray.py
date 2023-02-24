f768f7db2fa28b6491217754a3ec146711cb997d
Ubuntu <ubuntu@ip-172-31-2-150.ec2.internal>
2023-02-24 20:08:09 +0000

    Subiendo archivos

---
+++ menorArray.py
@@ -0,0 +1,10 @@
+def menor_de_arreglo(arreglo): 
+	
+    menor = arreglo[0]
+    
+    for elemento in arreglo:
+        if elemento < menor:
+            menor = elemento
+            
+    return menor
+    
---
+++ test_menorArray.py
@@ -0,0 +1,6 @@
+from menorArray import menor_de_arreglo
+
+
+def test_menor_de_arreglo():
+            
+    assert menor_de_arreglo([1,2,4,3,5,0]) == 0
